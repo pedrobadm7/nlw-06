654e5a05e6b3bc328df4a2a6da4ad01ae6566a2d
Pedro Barros <pedrobars7a@gmail.com>
2022-03-24 23:43:24 -0300

    format

--- src/server.ts
+++ src/server.ts
@@ -16,13 +16,13 @@ app.use((error: Error, request: Request, response: Response, next: NextFunction)
   if (error instanceof Error) {
     return response.status(400).json({
       error: error.message,
-    })
+    });
   }
 
   return response.status(500).json({
-    status: "error",
-    message: "Internal Server Error"
-  })
-})
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
 
-app.listen(3000, () => console.log("Server is running on port 3000 🚀 "));
+app.listen(3000, () => console.log('Server is running on port 3000 🚀 '));
