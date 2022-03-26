6e66db9fd72136fd822d4f1a0e8646fb41789ab8
Pedro Barros <pedrobars7a@gmail.com>
2022-03-26 10:01:26 -0300

    Add new routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,14 +2,20 @@ import { Router } from 'express';
 import { CreateUserController } from './controllers/CreateUserController';
 import { CreateTagController } from './controllers/CreateTagController';
 import { ensureAdmin } from './middlewares/ensureAdmin';
+import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { CreateComplimentController } from './controllers/CreateComplimentController';
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 
 router.use(ensureAdmin);
 router.post('/users', ensureAdmin, createUserController.handle);
 router.post('/tags', createTagController.handle);
+router.post('/login', authenticateUserController.handle);
+router.post('/compliments', createComplimentController.handle )
 
 export { router };
